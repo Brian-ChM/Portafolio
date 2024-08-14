38be64f2d992eb8974320feaf106b0e586c6a895
Brian Chaparro <brianmarecco@gmail.com>
2024-08-14 01:29:49 -0400

    Refactor and fix errors

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ app.post("/contact", (req, res) => {
     text: `Email: ${email}\nMensaje: ${message}`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  transporter.sendMail(mailOptions, (error) => {
     if (error) {
       console.log(error);
       return res.status(500).json({ error: "Error al enviar el mensaje" });
