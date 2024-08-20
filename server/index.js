3ed814f89e1f4bea011b5f42fa130dc12db88fe8
Brian Chaparro <brianmarecco@gmail.com>
2024-08-19 23:58:48 -0400

    Refactorización y cambio de diseño

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept",
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
